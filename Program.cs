c9f3b0a7dc97381a9a5e966b111874337cda8614
Aarron Szalacinski <aarron.szalacinski@happyappsoftware.com>
2019-11-10 20:30:54 -0500

    Moves KeyVault configs to Env vars

--- Program.cs
+++ Program.cs
@@ -22,22 +22,21 @@ namespace HAS.Content
                     var config = builder.Build();
 
                     builder.AddAzureKeyVault(
-                        $"https://{config["Azure:KeyVault:MPY:Vault"]}.vault.azure.net/",
-                        config["Azure:KeyVault:MPY:ClientId"],
-                        config["Azure:KeyVault:MPY:ClientSecret"]
-                        );
+                            $"https://{config["Azure.KeyVault.MPY.Vault"]}.vault.azure.net/",
+                            config["Azure.KeyVault.MPY.ClientId"],
+                            config["Azure.KeyVault.MPY.ClientSecret"]
+                            );
 
                     builder.AddAzureKeyVault(
-                        $"https://{config["Azure:KeyVault:HAS:Vault"]}.vault.azure.net/",
-                        config["Azure:KeyVault:HAS:ClientId"],
-                        config["Azure:KeyVault:HAS:ClientSecret"]
+                        $"https://{config["Azure.KeyVault.HAS.Vault"]}.vault.azure.net/",
+                        config["Azure.KeyVault.HAS.ClientId"],
+                        config["Azure.KeyVault.HAS.ClientSecret"]
                         );
 
                     if (ctx.HostingEnvironment.IsDevelopment())
                     {
                         builder.AddUserSecrets<Startup>();
                     }
-
 
                 })
                 .ConfigureWebHostDefaults(webBuilder =>
